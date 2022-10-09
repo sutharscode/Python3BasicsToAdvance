594f847bbbaa5a1c1c75645a36a654067262edf5
Harshil Suthar <85291021+sutharscode@users.noreply.github.com>
2022-10-09 15:13:01 +0530

    Add files via upload
    A Python code for Inheritance example.

---
+++ Inheritance_prog.py
@@ -0,0 +1,12 @@
+class Animal:
+    def __init__(self):
+        print("I am an Animal")
+    def speak():
+        print("i am a sub class from Animal")
+
+class Cat(Animal):
+    def speak():
+        print("cat speak Meow")
+
+ob=Cat()
+ob.speak()
