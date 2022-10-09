d320839748237a18c70a83838e978daf9127b81f
Harshil Suthar <85291021+sutharscode@users.noreply.github.com>
2022-10-09 15:30:01 +0530

    Add files via upload
    A Python file swapping the number

---
+++ swap.py
@@ -0,0 +1,11 @@
+x=int(input("Enter the x Value:"))
+print("now X is :",x)
+y=int(input("Enter the y Value:"))
+print("now Y is :",y)
+print("after swap")
+z=x
+x=y
+y=z
+
+print("After swap X is :",x)
+print("After swap y is :",y)
